91539d84da5b6517a2eec75b3994a3659e629e4d
geleeroyale <krassvs@gmail.com>
2020-03-19 02:02:45 +0100

    change chatroom

--- src/components/integrations/ChatInstance.js
+++ src/components/integrations/ChatInstance.js
@@ -7,7 +7,7 @@ function ChatInstance({ width, height, backgroundColor }) {
 			type='text/html'
 			width='800'
 			height='800'
-			src='https://messenger.interspace.chat/channel/general?layout=embedded'
+			src='https://messenger.interspace.chat/channel/main.space?layout=embedded'
 			frameborder='0'></iframe>
 	);
 }
