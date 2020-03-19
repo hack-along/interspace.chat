16e02209dde6cb289cfdb38df3e345d1ae84b9aa
geleeroyale <krassvs@gmail.com>
2020-03-20 00:02:30 +0100

    dark theme for chat embed

--- src/components/integrations/ChatInstance.js
+++ src/components/integrations/ChatInstance.js
@@ -3,7 +3,7 @@ import React from 'react';
 function ChatInstance({ width, height, backgroundColor }) {
 	return (
 		<iframe
-			src='https://titanembeds.com/embed/690315811293888778'
+			src='https://titanembeds.com/embed/690315811293888778?&theme=DiscordDark'
 			height='600'
 			width='800'
 			frameborder='0'></iframe>
