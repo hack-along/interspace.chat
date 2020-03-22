12bf4c13e23515a162ef77fcdfe9aeac8db9fb60
geleeroyale <krassvs@gmail.com>
2020-03-22 16:02:20 +0100

    add supporters

---
+++ src/components/Support.js
@@ -0,0 +1,105 @@
+import React from "react";
+import styled from "styled-components";
+import Collapsible from "react-collapsible";
+
+import SupporterParallele from "../img/support-parallele.png";
+import SupporterEcosystem from "../img/support-ecosystem.png";
+import SupporterEthglobal from "../img/support-ethglobal.png";
+import SupporterGitcoin from "../img/support-gitcoin.png";
+
+const SupportStyled = styled.nav`
+  text-align: left;
+
+  .Collapsible__trigger {
+    color: black;
+    background-color: whitesmoke;
+    cursor: pointer;
+    padding: 0.2rem;
+    :hover {
+      background-color: #00ffbf;
+    }
+  }
+  .Collapsible__contentOuter {
+    max-width: 300px;
+
+    :hover {
+      .Collapsible__contentInner {
+      }
+    }
+  }
+  .Collapsible__contentInner {
+    padding: 0.5rem;
+    background-color: whitesmoke;
+
+    p {
+      color: black;
+    }
+
+    a {
+      color: #00ffbf;
+    }
+  }
+`;
+
+const StyledListItem = styled.li`
+  list-style-type: none;
+  display: flex;
+  padding: 1rem;
+  color: black;
+`;
+
+const StyledLink = styled.a`
+  display: grid;
+  grid-template-columns: auto auto;
+  text-decoration-line: none;
+`;
+
+const StyledSupportertext = styled.span`
+  align-self: center;
+  color: black;
+  padding: 1rem;
+`;
+
+const Support = () => {
+  return (
+    <SupportStyled>
+      <Collapsible trigger="Support">
+        <p>Supported by:</p>
+        <ul>
+          <StyledListItem>
+            <StyledLink href="https://parallele.at">
+              <img src={SupporterParallele} />
+
+              <StyledSupportertext>Parallele Polis</StyledSupportertext>
+            </StyledLink>
+          </StyledListItem>
+          <StyledListItem>
+            <StyledLink href="https://ecosystem.support">
+              <img src={SupporterEcosystem} />
+
+              <StyledSupportertext>
+                Ethereum Ecosystem Support
+              </StyledSupportertext>
+            </StyledLink>
+          </StyledListItem>
+          <StyledListItem>
+            <StyledLink href="https://www.ethglobal.co">
+              <img src={SupporterEthglobal} />
+
+              <StyledSupportertext>ETHglobal</StyledSupportertext>
+            </StyledLink>
+          </StyledListItem>
+          <StyledListItem>
+            <StyledLink href="https://gitcoin.co">
+              <img src={SupporterGitcoin} />
+
+              <StyledSupportertext>Gitcoin</StyledSupportertext>
+            </StyledLink>
+          </StyledListItem>
+        </ul>
+      </Collapsible>
+    </SupportStyled>
+  );
+};
+
+export default Support;
