8d33bfe6251b04193624774ea505d78d918c8d76
geleeroyale <krassvs@gmail.com>
2020-03-23 20:43:07 +0100

    change chat instance width/height

--- src/components/integrations/ChatInstance.js
+++ src/components/integrations/ChatInstance.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
-function ChatInstance({ width, height, backgroundColor }) {
-	return (
-		<iframe
-			src='https://titanembeds.com/embed/690315811293888778?&theme=DiscordDark'
-			height='600'
-			width='800'
-			frameborder='0'></iframe>
-	);
+function ChatInstance({ width, height }) {
+  return (
+    <iframe
+      src="https://titanembeds.com/embed/690315811293888778?&theme=DiscordDark"
+      width={width}
+      height={height}
+      frameborder="0"
+      title="chat"
+    ></iframe>
+  );
 }
 
 export default ChatInstance;
