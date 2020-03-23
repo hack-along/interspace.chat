b31915a29a1947bed690c1c3440d501edf518d0f
geleeroyale <krassvs@gmail.com>
2020-03-23 20:42:06 +0100

    add calendar

--- src/components/FloatingRoomWindow.js
+++ src/components/FloatingRoomWindow.js
@@ -1,181 +1,204 @@
 import React, {
-	Fragment,
-	useEffect,
-	useContext,
-	useState,
-	useRef
-} from 'react';
-import ReactDOM from 'react-dom';
-import styled from 'styled-components';
-import Draggable from 'react-draggable';
+  Fragment,
+  useEffect,
+  useContext,
+  useState,
+  useRef
+} from "react";
+import ReactDOM from "react-dom";
+import styled from "styled-components";
+import Draggable from "react-draggable";
 
-import { FloatingSpaceContext } from '../contexts/FloatingSpaceContext';
-import LoftRadioInstance from './integrations/LoftRadioInstance';
-import ChatInstance from './integrations/ChatInstance';
-import YoutubeInstance from './integrations/YoutubeInstance';
-import RTreesInstance from './integrations/RTreesInstance';
-import RoomInstance from './RoomInstance';
-import { RoomNames } from '../utils/constants';
+import { FloatingSpaceContext } from "../contexts/FloatingSpaceContext";
+import LoftRadioInstance from "./integrations/LoftRadioInstance";
+import ChatInstance from "./integrations/ChatInstance";
+import CalendarInstance from "./integrations/CalendarInstance";
+import YoutubeInstance from "./integrations/YoutubeInstance";
+// import RTreesInstance from "./integrations/RTreesInstance";
+import RoomInstance from "./RoomInstance";
+import { RoomNames } from "../utils/constants";
 
-const height = 600;
-const width = 850;
+const width = window.innerWidth / 2;
+const height = window.innerHeight / 2;
 const padding = 15;
 const innerWidth = width - padding * 2;
 let zIndexIterator = 1000;
 
 const FloatingRoomWindowContainer = styled.div`
-	width: ${width}px;
-	position: absolute;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
-	pointer-events: none;
+  width: ${width}px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  pointer-events: none;
 `;
 
 const FloatingRoomWindowCloser = styled.div`
-	width: 18px;
-	height: 30px;
-	opacity: 0.7;
-	transition: all 0.2s ease-in-out;
-	cursor: pointer;
-	&:hover {
-		opacity: 1;
-	}
-	&::before {
-		content: '×'; // here is your X(cross) sign.
-		color: #fff;
-		font-family: Arial, sans-serif;
-		font-weight: bold;
-		font-size: 30px;
-	}
+  width: 18px;
+  height: 30px;
+  opacity: 0.7;
+  transition: all 0.2s ease-in-out;
+  cursor: pointer;
+  &:hover {
+    opacity: 1;
+  }
+  &::before {
+    content: "×"; // here is your X(cross) sign.
+    color: #fff;
+    font-family: Arial, sans-serif;
+    font-weight: bold;
+    font-size: 30px;
+  }
 `;
 
 const DraggableController = styled.div`
-	padding: 15px;
-	padding-top: 0px;
-	background-color: #3e3d3deb;
-	border-radius: 10;
-	cursor: all-scroll;
-	pointer-events: all;
-	box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
-		0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
-	&:active iframe {
-		pointer-events: all;
-		cursor: unset;
-	}
+  padding: 15px;
+  padding-top: 0px;
+  background-color: #3e3d3deb;
+  border-radius: 10;
+  cursor: all-scroll;
+  pointer-events: all;
+  box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
+    0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+  &:active iframe {
+    pointer-events: all;
+    cursor: unset;
+  }
 `;
 
 function FloatingRoomWindow() {
-	const { currentFloatingSpaces, setFloatingSpaces } = useContext(
-		FloatingSpaceContext
-	);
-	const [floatingRoomWindows, setFloatingRoomWindow] = useState(
-		currentFloatingSpaces
-	);
-	const [focusedWindow, setFocusedWindow] = useState(null);
-	const windowFrame = useRef();
+  const { currentFloatingSpaces, setFloatingSpaces } = useContext(
+    FloatingSpaceContext
+  );
+  const [floatingRoomWindows, setFloatingRoomWindow] = useState(
+    currentFloatingSpaces
+  );
+  const [focusedWindow, setFocusedWindow] = useState(null);
+  const windowFrame = useRef();
 
-	useEffect(() => {
-		let useFloatingRoomWindow = getFloatingRoomWindow(currentFloatingSpaces);
-		setFloatingRoomWindow(useFloatingRoomWindow);
-	}, [currentFloatingSpaces]);
+  useEffect(() => {
+    let useFloatingRoomWindow = getFloatingRoomWindow(currentFloatingSpaces);
+    setFloatingRoomWindow(useFloatingRoomWindow);
+  }, [currentFloatingSpaces]);
 
-	useEffect(() => {
-		// Sets the focused window to the incremented
-		if (windowFrame.current) {
-			let newWindow = ReactDOM.findDOMNode(
-				windowFrame.current
-			).parentNode.querySelector(`[data-window="window-${focusedWindow}"]`);
-			if (newWindow) {
-				ReactDOM.findDOMNode(windowFrame.current).parentNode.querySelector(
-					`[data-window="window-${focusedWindow}"]`
-				).style.zIndex = zIndexIterator++;
-			}
-		}
-	}, [focusedWindow]);
+  useEffect(() => {
+    // Sets the focused window to the incremented
+    if (windowFrame.current) {
+      let newWindow = ReactDOM.findDOMNode(
+        windowFrame.current
+      ).parentNode.querySelector(`[data-window="window-${focusedWindow}"]`);
+      if (newWindow) {
+        ReactDOM.findDOMNode(windowFrame.current).parentNode.querySelector(
+          `[data-window="window-${focusedWindow}"]`
+        ).style.zIndex = zIndexIterator++;
+      }
+    }
+  }, [focusedWindow]);
 
-	const getFloatingRoomWindow = currentFloatingSpaces => {
-		let newFloatingRooms = [];
-		if (currentFloatingSpaces) {
-			for (let currentSpace of currentFloatingSpaces) {
-				if (currentSpace === 'loft.radio') {
-					newFloatingRooms.push({
-						key: currentSpace,
-						element: (
-							<LoftRadioInstance
-								width={innerWidth}
-								height={height}></LoftRadioInstance>
-						)
-					});
-				} else if (currentSpace === 'chat') {
-					newFloatingRooms.push({
-						key: currentSpace,
-						element: (
-							<ChatInstance width={innerWidth} height={height}></ChatInstance>
-						)
-					});
-				} else if (RoomNames.indexOf(currentSpace) > -1) {
-					newFloatingRooms.push({
-						key: currentSpace,
-						element: (
-							<RoomInstance
-								width={innerWidth}
-								space={currentSpace}
-								height={height}
-							/>
-						)
-					});
-				} else if (currentSpace === null) {
-					newFloatingRooms.push(null);
-				}
-				if (currentSpace !== null) {
-					setFocusedWindow(currentSpace);
-				}
-			}
-		}
-		return newFloatingRooms.length > 0 ? newFloatingRooms : null;
-	};
+  const getFloatingRoomWindow = currentFloatingSpaces => {
+    let newFloatingRooms = [];
+    if (currentFloatingSpaces) {
+      for (let currentSpace of currentFloatingSpaces) {
+        if (currentSpace === "loft.radio") {
+          newFloatingRooms.push({
+            key: currentSpace,
+            element: (
+              <LoftRadioInstance
+                width={innerWidth}
+                height={height}
+              ></LoftRadioInstance>
+            )
+          });
+        } else if (currentSpace === "chat") {
+          newFloatingRooms.push({
+            key: currentSpace,
+            element: (
+              <ChatInstance width={innerWidth} height={height}></ChatInstance>
+            )
+          });
+        } else if (currentSpace === "calendar") {
+          newFloatingRooms.push({
+            key: currentSpace,
+            element: (
+              <CalendarInstance
+                width={innerWidth}
+                height={height}
+              ></CalendarInstance>
+            )
+          });
+        } else if (currentSpace === "youtube") {
+          newFloatingRooms.push({
+            key: currentSpace,
+            element: (
+              <YoutubeInstance
+                width={innerWidth}
+                height={height}
+              ></YoutubeInstance>
+            )
+          });
+        } else if (RoomNames.indexOf(currentSpace) > -1) {
+          newFloatingRooms.push({
+            key: currentSpace,
+            element: (
+              <RoomInstance
+                width={innerWidth}
+                space={currentSpace}
+                height={height}
+              />
+            )
+          });
+        } else if (currentSpace === null) {
+          newFloatingRooms.push(null);
+        }
+        if (currentSpace !== null) {
+          setFocusedWindow(currentSpace);
+        }
+      }
+    }
+    return newFloatingRooms.length > 0 ? newFloatingRooms : null;
+  };
 
-	const closeFloatingRoomWindow = windowKey => {
-		if (currentFloatingSpaces.indexOf(windowKey) > -1) {
-			let newFloatingSpaces = [...currentFloatingSpaces];
-			// Replaces the floating window with a null record, to prevent the existing windows from shifting position
-			newFloatingSpaces.splice(newFloatingSpaces.indexOf(windowKey), 1, null);
-			setFloatingSpaces(newFloatingSpaces);
-			setFocusedWindow(null);
-		}
-	};
+  const closeFloatingRoomWindow = windowKey => {
+    if (currentFloatingSpaces.indexOf(windowKey) > -1) {
+      let newFloatingSpaces = [...currentFloatingSpaces];
+      // Replaces the floating window with a null record, to prevent the existing windows from shifting position
+      newFloatingSpaces.splice(newFloatingSpaces.indexOf(windowKey), 1, null);
+      setFloatingSpaces(newFloatingSpaces);
+      setFocusedWindow(null);
+    }
+  };
 
-	let setWindowFocus = event => {
-		ReactDOM.findDOMNode(
-			event.target
-		).parentNode.style.zIndex = zIndexIterator++;
-	};
+  let setWindowFocus = event => {
+    ReactDOM.findDOMNode(
+      event.target
+    ).parentNode.style.zIndex = zIndexIterator++;
+  };
 
-	return (
-		<Fragment>
-			{floatingRoomWindows &&
-				floatingRoomWindows.length >= 1 &&
-				floatingRoomWindows.map(floatingWindow =>
-					floatingWindow && floatingWindow.key && floatingWindow.element ? (
-						<FloatingRoomWindowContainer
-							key={floatingWindow.key}
-							ref={windowFrame}
-							onMouseDown={event => setWindowFocus(event)}
-							data-window={`window-${floatingWindow.key}`}>
-							<Draggable>
-								<DraggableController>
-									<FloatingRoomWindowCloser
-										onClick={() => closeFloatingRoomWindow(floatingWindow.key)}
-									/>
-									{floatingWindow.element}
-								</DraggableController>
-							</Draggable>
-						</FloatingRoomWindowContainer>
-					) : null
-				)}
-		</Fragment>
-	);
+  return (
+    <Fragment>
+      {floatingRoomWindows &&
+        floatingRoomWindows.length >= 1 &&
+        floatingRoomWindows.map(floatingWindow =>
+          floatingWindow && floatingWindow.key && floatingWindow.element ? (
+            <FloatingRoomWindowContainer
+              key={floatingWindow.key}
+              ref={windowFrame}
+              onMouseDown={event => setWindowFocus(event)}
+              data-window={`window-${floatingWindow.key}`}
+            >
+              <Draggable>
+                <DraggableController>
+                  <FloatingRoomWindowCloser
+                    onClick={() => closeFloatingRoomWindow(floatingWindow.key)}
+                  />
+                  {floatingWindow.element}
+                </DraggableController>
+              </Draggable>
+            </FloatingRoomWindowContainer>
+          ) : null
+        )}
+    </Fragment>
+  );
 }
 
 export default FloatingRoomWindow;
---
+++ src/components/integrations/CalendarInstance.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function CalendarInstance({ width, height }) {
+  return (
+    <iframe
+      src="https://calendar.google.com/calendar/b/1/embed?height=400&wkst=1&bgcolor=%23e3e9ff&ctz=Europe%2FVienna&src=N3RtbnFxamZtdnB2dGIyNTdycDNlNDkxbmtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23F4511E&showPrint=0&showDate=1&mode=AGENDA&showNav=1&showTabs=1&showCalendars=1"
+      width={width}
+      height={height}
+      frameborder="0"
+      scrolling="no"
+      title="calendar"
+    ></iframe>
+  );
+}
+
+export default CalendarInstance;
