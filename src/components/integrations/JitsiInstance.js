a02a7fe837cb6408c80df02aabd14ade7bcc07c9
geleeroyale <krassvs@gmail.com>
2020-03-23 20:54:36 +0100

    override some jitsi settings

--- src/components/integrations/JitsiInstance.js
+++ src/components/integrations/JitsiInstance.js
@@ -1,27 +1,58 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 // import * as JitsiMeetExternalAPI from "../dist/jitsi";
 
 const JitsiMeetExternalAPI = window.JitsiMeetExternalAPI;
 
 const JitsiInstance = ({ width, height, roomData }) => {
-	useEffect(() => {
-		const options = {
-			roomName: roomData.roomName,
-			width: width,
-			height: height,
-			interfaceConfigOverwrite: { defaultLanguage: `de` },
-			parentNode: document.querySelector('#meet')
-		};
-		const api = new JitsiMeetExternalAPI(roomData.domain, options);
+  useEffect(() => {
+    const options = {
+      roomName: roomData.roomName,
+      width: width,
+      height: height,
+      interfaceConfigOverwrite: { defaultLanguage: `de` },
+      interfaceConfigOverwrite: { TOOLBAR_ALWAYS_VISIBLE: "true" },
+      interfaceConfigOverwrite: { INITIAL_TOOLBAR_TIMEOUT: 200000 },
+      interfaceConfigOverwrite: {
+        TOOLBAR_BUTTONS: [
+          "microphone",
+          "camera",
+          "closedcaptions",
+          "desktop",
+          "fullscreen",
+          "fodeviceselection",
+          "hangup",
+          "profile",
+          "info",
+          "recording",
+          "livestreaming",
+          "etherpad",
+          "sharedvideo",
+          "settings",
+          "raisehand",
+          "videoquality",
+          "filmstrip",
+          "invite",
+          "feedback",
+          "stats",
+          "shortcuts",
+          "tileview",
+          "download",
+          "help",
+          "mute-everyone"
+        ]
+      },
+      parentNode: document.querySelector("#meet")
+    };
+    const api = new JitsiMeetExternalAPI(roomData.domain, options);
 
-		// api.executeCommand("displayName", user);
+    // api.executeCommand("displayName", user);
 
-		return function cleanup() {
-			api.dispose();
-		};
-	}, [roomData, width, height]);
+    return function cleanup() {
+      api.dispose();
+    };
+  }, [roomData, width, height]);
 
-	return <div id='meet'></div>;
+  return <div id="meet"></div>;
 };
 
 export default JitsiInstance;
