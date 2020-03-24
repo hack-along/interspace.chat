46f0ed62fb9dae40383fca65ee24e08ccded70cd
geleeroyale <krassvs@gmail.com>
2020-03-24 16:40:08 +0100

    remove chat provider logo

--- src/components/integrations/ChatInstance.js
+++ src/components/integrations/ChatInstance.js
@@ -1,9 +1,10 @@
 import React from "react";
+import styled from "styled-components";
 
 function ChatInstance({ width, height }) {
   return (
     <iframe
-      src="https://titanembeds.com/embed/690315811293888778?&theme=DiscordDark"
+      src="https://titanembeds.com/embed/690315811293888778?css=85&theme=DiscordDark"
       width={width}
       height={height}
       frameborder="0"
