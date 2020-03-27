c77f9f3d453fc96b866b5c92948f844433ee349a
geleeroyale <krassvs@gmail.com>
2020-03-27 15:40:02 +0100

    change calendar view to show all tracks

--- src/components/integrations/CalendarInstance.js
+++ src/components/integrations/CalendarInstance.js
@@ -3,7 +3,7 @@ import React from "react";
 function CalendarInstance() {
   return (
     <iframe
-      src="https://calendar.google.com/calendar/b/1/embed?height=400&wkst=1&bgcolor=%23e3e9ff&ctz=Europe%2FVienna&src=N3RtbnFxamZtdnB2dGIyNTdycDNlNDkxbmtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23F4511E&showPrint=0&showDate=1&mode=AGENDA&showNav=1&showTabs=1&showCalendars=1"
+      src="https://calendar.google.com/calendar/b/1/embed?height=400&wkst=2&bgcolor=%23ffffff&ctz=Europe%2FVienna&src=N3RtbnFxamZtdnB2dGIyNTdycDNlNDkxbmtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=aWo1cmVzcGM2bHFpN2Y5ZWVkYmcxZWlkbnNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=NDgzY3RzbjZkZzIydWp0NW9uczlvOHBmcHNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=bDRxbGtzZDgyZDJhZzFia2ljOHB1NDRxbGdAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23000000&color=%234285F4&color=%238E24AA&color=%23F4511E&mode=AGENDA"
       width="100%"
       height="100%"
       frameborder="0"
